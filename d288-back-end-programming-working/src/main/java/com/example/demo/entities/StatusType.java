0148151f00f8a6f0a6431faecf53d1dac1fafa6a
Shane Bogue <90866675+SerotoninShane@users.noreply.github.com>
2025-05-12 05:55:35 -0700

    Delete d288-back-end-programming-working directory

--- d288-back-end-programming-working/src/main/java/com/example/demo/entities/StatusType.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.entities; // Use the appropriate package
-
-public enum StatusType {
-    pending,
-    ordered,
-    canceled
-}
--- d288-back-end-programming-working/src/main/java/com/example/demo/services/CheckoutService.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.services;
-
-public interface CheckoutService {
-    PurchaseResponse placeOrder(Purchase purchase); // Method to place an order
-    void checkout(Purchase purchase); // Method to handle the checkout process
-}
